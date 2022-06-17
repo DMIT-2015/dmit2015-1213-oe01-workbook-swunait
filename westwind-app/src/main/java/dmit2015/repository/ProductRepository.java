b29a46f9776995577a4646e071f85f8e5d061521
Sam Wu <swu@nait.ca>
2022-06-16 19:45:30 -0600

    Add partial search example

--- westwind-app/src/main/java/dmit2015/repository/ProductRepository.java
+++ westwind-app/src/main/java/dmit2015/repository/ProductRepository.java
@@ -5,12 +5,25 @@ import dmit2015.entity.Product;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
+import java.util.List;
+
 @ApplicationScoped
 @Transactional
 public class ProductRepository extends AbstractJpaRepository<Product, Integer> {
 
     public ProductRepository() {
         super(Product.class);
+    }
+
+    public List<Product> findByProductName(String partialProductName) {
+        return getEntityManager()
+                .createQuery("""
+                    SELECT p
+                    FROM Product p
+                    WHERE p.productName LIKE :productNameValue
+                    """, Product.class)
+                .setParameter("productNameValue", "%" + partialProductName + "%")
+                .getResultList();
     }
 
 }
--- westwind-app/src/main/java/dmit2015/view/ProductListController.java
+++ westwind-app/src/main/java/dmit2015/view/ProductListController.java
@@ -3,6 +3,7 @@ package dmit2015.view;
 import dmit2015.entity.Product;
 import dmit2015.repository.ProductRepository;
 import lombok.Getter;
+import lombok.Setter;
 import org.omnifaces.util.Messages;
 
 import jakarta.annotation.PostConstruct;
@@ -26,10 +27,23 @@ public class ProductListController implements Serializable {
 
     @PostConstruct  // After @Inject is complete
     public void init() {
+//        try {
+//            productList = _productRepository.findAll();
+//        } catch (Exception ex) {
+//            Messages.addGlobalError(ex.getMessage());
+//        }
+    }
+
+    @Getter @Setter
+    private String productNameSearchValue;
+    public void doSearchByProductName() {
         try {
-            productList = _productRepository.findAll();
+            productList = _productRepository.findByProductName(productNameSearchValue);
+            Messages.addGlobalInfo("Search returned {0} results.", productList.size());
         } catch (Exception ex) {
-            Messages.addGlobalError(ex.getMessage());
+            ex.printStackTrace();
+            Messages.addGlobalError("Error searching for results. {0}", ex.getMessage());
         }
+
     }
 }
