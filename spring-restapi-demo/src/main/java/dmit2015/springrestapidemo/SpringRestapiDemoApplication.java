319ab7f6ce2e746b951cb42b18704257c21f4ba1
Sam Wu <swu@nait.ca>
2022-08-04 07:32:07 -0600

    fixed spelling error

---
+++ spring-restapi-demo/src/main/java/dmit2015/springrestapidemo/SpringRestapiDemoApplication.java
@@ -0,0 +1,13 @@
+package dmit2015.springrestapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestapiDemoApplication.class, args);
+    }
+
+}
---
+++ spring-restapi-demo/src/test/java/dmit2015/springrestapidemo/SpringRestapiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dmit2015.springrestapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestapiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
